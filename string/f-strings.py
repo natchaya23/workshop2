74a1b1ca04c785b658c0230d01b0144981b9325f
DESKTOP-DHHTU1I\natch <natchaya.no@ku.th>
2021-01-16 10:50:43 +0700

    add string

---
+++ string/f-strings.py
@@ -0,0 +1,7 @@
+name = "nam"
+age = 22
+
+result = f" My name is {name}, and I am {age}"
+
+
+print("result : ", result)  # Output:"My name is nam, and I am 26"
---
+++ string/format.py
@@ -0,0 +1,6 @@
+age = 26
+txt = "My name is nam, and I am {}"
+
+result = txt.format(age)
+
+print("result: ", result)  # Output : "My name is nam, and I am 26"
